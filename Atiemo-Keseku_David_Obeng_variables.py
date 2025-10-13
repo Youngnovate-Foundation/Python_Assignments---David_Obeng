562981ee1859f848ab50f408927e6194de6b018a
Atiemo-Keseku David Obeng <datiemokeseku16@gmail.com>
2025-10-13 16:54:42 +0000

    Initial Commit

---
+++ Atiemo-Keseku_David_Obeng_variables.py
@@ -0,0 +1,35 @@
+#Exercise 1
+name = "Atiemo-Keseku David Obeng"
+age = 20
+favProgrammingLang = "Javascript"
+complete = True
+
+#Exercise 2
+num1, num2 = 15, 4
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+#Exercise 3
+first_name, last_name = "David" , "Atiemo-Keseku"
+full_name = first_name + " " + last_name
+print(full_name)
+print(full_name.upper())
+print(f'Hello, my name is {full_name}.')
+
+#Exercise 4
+fav_food = input("What is your favorite food? ")
+timesPerWeek = input("How many times do you eat per week? ")
+print("Hello! Your favorite food is " + fav_food + " and you eat " + timesPerWeek + " times per week")
+
+#Exercise 5
+age = "25"
+height = 5.8
+convertedAge = int(age)
+print(f"I am {convertedAge} years old and {height} feet tall")
+
+#Exercise 6
+c = int(input("Input temperature in celcius: "))
+f = (c * (9/5) ) + 32
+print(f'{c} degrees celsius converted to farenheit is {f} degrees fahrenheit') 
