10d8ca6c815c7e73ce23eddfeb12fcf24dfc0da8
Atiemo-Keseku David Obeng <datiemokeseku16@gmail.com>
2025-10-16 22:46:44 +0000

    Update graduate student class

--- David_Obeng_OOP.py
+++ David_Obeng_OOP.py
@@ -2,10 +2,14 @@ class Student:
     def __init__(self, name, age, grade):
         self.name = name
         self.age = age
-        self.grade = grade
+        self.__grade = grade
 
     def display_info(self):
         print(f'Name: {self.name}, age: {self.age} and Grade: {self.grade}.')
 
 class GraduateStudent(Student):
-    pass
+    def __init__(self, name, age, grade, degree):
+        super().__init__(name, age, grade)
+        self.degree = degree
+
+    
