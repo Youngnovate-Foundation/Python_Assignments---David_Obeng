898a5615db289909b6f82ec8f063c79e179fff66
Atiemo-Keseku David Obeng <datiemokeseku16@gmail.com>
2025-10-17 01:46:55 +0000

    Create objects for various classes and print results

--- David_Obeng_OOP.py
+++ David_Obeng_OOP.py
@@ -23,4 +23,27 @@ class GraduateStudent(Student):
 
     # Overiding display Info method.
     def display_info(self):
-        return f'Graduate Student - Name: {self.name}, Age: {self.age}, Grade: {self.grade} and Degree: {self.degree}.'
+        return f'Graduate Student - Name: {self.name}, Age: {self.age}, Grade: {self._Student__grade} and Degree: {self.degree}.'
+    
+
+# Objects 
+std1 = Student("Kwame Ansah", 21, "C")
+std2 = Student("Sarah Odom", 25, "D")
+std3 = Student("Samuel Sam", 24, "B")
+grad1 = GraduateStudent("David Obeng", 23, "A", "Doctor of Science")
+grad2 = GraduateStudent("Amy Oteng", 24, "C", "Bachelor of Arts")
+
+# Displaying Info
+print(std1.display_info())
+print(grad1.display_info())
+
+#Getter and Setter Methods
+print(std2.get_grade())
+std2.set_grade("A")
+print(std2.get_grade())
+
+# Polymorphysm
+students = [std1, std2, std3, grad1, grad2]
+
+for student in students:
+    print(student.display_info())
