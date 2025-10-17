f36ff788b0cabef0ffcabcaeaab3e25cd96c7745
Atiemo-Keseku David Obeng <datiemokeseku16@gmail.com>
2025-10-17 01:04:12 +0000

    Create a getter function

--- David_Obeng_OOP.py
+++ David_Obeng_OOP.py
@@ -4,12 +4,20 @@ class Student:
         self.age = age
         self.__grade = grade
 
-    def display_info(self):
-        print(f'Name: {self.name}, age: {self.age} and Grade: {self.grade}.')
+    # Getter method
+    def get_grade(self):
+        return self.__grade
 
+    # Display Student info
+    def display_info(self):
+        return f'Name: {self.name}, age: {self.age} and Grade: {self.__grade}.'
+
+# Gruaduate Student Class
 class GraduateStudent(Student):
     def __init__(self, name, age, grade, degree):
         super().__init__(name, age, grade)
         self.degree = degree
 
-    
+    # Overiding display Info method.
+    def display_info(self):
+        return f'Graduate Student - Name: {self.name}, Age: {self.age}, Grade: {self.grade} and Degree: {self.degree}.'
